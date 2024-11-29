52d0ff7a7faf0382d27d502a4d77fb44f67bf337
leofang2992 <billyfang.frontend@gmail.com>
2024-11-29 13:29:51 +0800

    728 updater

---
+++ 728.self-dividing-numbers.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=728 lang=javascript
+ *
+ * [728] Self Dividing Numbers
+ */
+
+// @lc code=start
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    //1. loop 暴力计算
+    for( let i = left; i <= right; i++  ) {
+        let res = [];
+        if ( isSelfDividing(i) ) {
+            res.push(i);
+        }
+    }
+    return res;
+}
+
+const isSelfDividing = (num) => {
+    let temp = num;
+    while( temp > 0) {
+        const digit = temp %10;
+        if(digit === 0 || num % digit !== 0) {
+            return false;
+        }
+        temp = Math.floor(temp / 10);
+    }
+    return true;
+};
+
+
+// @lc code=end
+
