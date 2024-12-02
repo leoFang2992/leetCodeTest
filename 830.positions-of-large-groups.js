149ccc7f6956d1719df3127b42b9bcc74b7365f7
leofang2992 <billyfang.frontend@gmail.com>
2024-12-02 09:37:55 +0800

    update 830

---
+++ 830.positions-of-large-groups.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=830 lang=javascript
+ *
+ * [830] Positions of Large Groups
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number[][]}
+ */
+var largeGroupPositions = function(s) {
+    
+};
+// @lc code=end
+
