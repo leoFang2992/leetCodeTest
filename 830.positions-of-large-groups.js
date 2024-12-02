12e83817cb4ea41f381f7fb95cc39307fa35e5fb
leofang2992 <billyfang.frontend@gmail.com>
2024-12-02 13:38:29 +0800

    finished 830

--- 830.positions-of-large-groups.js
+++ 830.positions-of-large-groups.js
@@ -10,7 +10,19 @@
  * @return {number[][]}
  */
 var largeGroupPositions = function(s) {
-    
+    //loop string  s[i] s[i+1]
+
+    let res = [];
+    let start = 0;
+    for( let i = 0 ; i< s.length; i++) {
+        if(s[i] != s[i+1]) {
+            if( i - start >= 2 ) {
+                res.push([start,i]);
+            }
+            start  = i + 1;
+        }
+    }
+    return res;
 };
 // @lc code=end
 
