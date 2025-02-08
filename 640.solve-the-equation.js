ef106737bb2794445e6ebbdfe3cedd54b6533dae
leofang2992 <billyfang.frontend@gmail.com>
2025-02-08 13:20:42 +0800

    update

---
+++ 640.solve-the-equation.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=640 lang=javascript
+ *
+ * [640] Solve the Equation
+ */
+
+// @lc code=start
+/**
+ * @param {string} equation
+ * @return {string}
+ */
+var solveEquation = function(equation) {
+    
+};
+// @lc code=end
+
