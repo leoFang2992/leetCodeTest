4c63a615267da7e80afb86a9100ad4723fd323b6
leofang2992 <billyfang.frontend@gmail.com>
2025-05-27 10:01:13 +0800

    790

---
+++ 790.domino-and-tromino-tiling.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=790 lang=javascript
+ *
+ * [790] Domino and Tromino Tiling
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTilings = function(n) {
+    
+  
+    
+};
+// @lc code=end
+
